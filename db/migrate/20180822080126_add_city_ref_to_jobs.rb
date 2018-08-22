fe60cf0528864dc8e0a848b5602c323c92d7ffe9
Phuc Thai Minh <phuctm@zigexn.vn>
2018-08-22 15:33:17 +0700

    Fix reviewed code

--- db/migrate/20180822080126_add_city_ref_to_jobs.rb
+++ db/migrate/20180822080126_add_city_ref_to_jobs.rb
@@ -1,5 +1,5 @@
 class AddCityRefToJobs < ActiveRecord::Migration[5.2]
   def change
-    add_reference :jobs, :city, foreign_key: true, index: true
+    add_reference :jobs, :city, index: true
   end
 end
